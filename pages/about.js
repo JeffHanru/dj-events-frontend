0d91cccd5570cd8e2bb469774a714a40a4881145
Hanru Xu <xjhzwx@gmail.com>
2022-09-10 12:14:36 +1000

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the lastest DJ and other musical events</p>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>add event</h1>
+    </div>
+  )
+}
